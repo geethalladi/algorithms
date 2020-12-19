5dc768fa7e0e6c40eb0897ab8673b9602a539b04
geethalladi <geeth.alladi@freshworks.com>
2020-12-19 23:55:26 +0530

    Updating a recursive binary search

--- algo/binary_search.py
+++ algo/binary_search.py
@@ -1,3 +1,8 @@
+"""
+Module containing a method to do binary_search
+"""
+
+
 def binary_search(elements, key, start, end):
     """
     Returns the index of the `key` inside the `elements`
@@ -5,5 +10,18 @@ def binary_search(elements, key, start, end):
     ascending order. Returns -1 when the key element is not
     found
     """
-    return -1
+    if((len(elements) <= 0) or (start > end)):
+        # when there are no elements return -1
+        return -1
 
+    mid = (start + end) / 2
+    if elements[mid] == key:
+        # add logger here
+        return mid
+
+    if key < elements[mid]:
+        # then find in the left half
+        return binary_search(elements, key, start, mid - 1)
+
+    # find in the right half
+    return binary_search(elements, key, mid + 1, end)

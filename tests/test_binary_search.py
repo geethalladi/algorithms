f86375e2b2b85732d3cca304fc5e25ab80b93ca5
geethalladi <geeth.alladi@freshworks.com>
2020-12-19 23:31:16 +0530

    Adding another unit test for binary search

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -6,3 +6,6 @@ from algo.binary_search import binary_search as search
 class TestBinarySearch:
     def test_empty(self):
         assert search([], 1, 0, 0) == -1
+
+    def test_not_present(self):
+        assert search([1, 2, 3, 4, 5], -2, 0, 4) == -1

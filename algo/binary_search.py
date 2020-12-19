5bf3ac26e72ea05fb494aa4768a9a3f09d4b7b6e
geethalladi <geeth.alladi@freshworks.com>
2020-12-20 00:22:25 +0530

    Making the binary search interface better

--- algo/binary_search.py
+++ algo/binary_search.py
@@ -2,14 +2,18 @@
 Module containing a method to do binary_search
 """
 
-
-def binary_search(elements, key, start, end):
+def binary_search(elements, key):
     """
     Returns the index of the `key` inside the `elements`
     list. The `elements` list is assumed to be sorted in
     ascending order. Returns -1 when the key element is not
     found
     """
+    start, end = 0, (len(elements) - 1)
+    return _binary_search(elements, key, start, end)
+
+
+def _binary_search(elements, key, start, end):
     if((len(elements) <= 0) or (start > end)):
         # when there are no elements return -1
         return -1
@@ -21,7 +25,7 @@ def binary_search(elements, key, start, end):
 
     if key < elements[mid]:
         # then find in the left half
-        return binary_search(elements, key, start, mid - 1)
+        return _binary_search(elements, key, start, mid - 1)
 
     # find in the right half
-    return binary_search(elements, key, mid + 1, end)
+    return _binary_search(elements, key, mid + 1, end)
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -8,20 +8,20 @@ from algo.binary_search import binary_search as search
 
 class TestBinarySearch:
     def test_empty(self):
-        assert search([], 1, 0, 0) == -1
+        assert search([], 1) == -1
 
     def test_not_present(self):
-        assert search([1, 2, 3, 4, 5], -2, 0, 4) == -1
+        assert search([1, 2, 3, 4, 5], -2) == -1
 
     def test_single_element(self):
-        assert search([1], 1, 0, 1) == 0
+        assert search([1], 1) == 0
 
     def test_multiple_elements_found(self):
-        assert search([1, 3, 5, 7, 9, 11], 3, 0, 5) == 1
+        assert search([1, 3, 5, 7, 9, 11], 3) == 1
 
     def test_multiple_elements_not_found(self):
-        assert search([1, 3, 5, 7, 9, 11], 12, 0, 5) == -1
+        assert search([1, 3, 5, 7, 9, 11], 12) == -1
 
     def test_multiple_elements_not_found2(self):
-        assert search([1, 3, 5, 7, 9, 11], -1, 0, 5) == -1
+        assert search([1, 3, 5, 7, 9, 11], -1) == -1
         

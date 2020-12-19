563a5c860a2c8b02677194541d23621135ca24a2
geethalladi <geeth.alladi@freshworks.com>
2020-12-19 22:23:21 +0530

    Creating a method for binary search

---
+++ algo/binary_search.py
@@ -0,0 +1,9 @@
+def binary_search(elements, key, start, end):
+    """
+    Returns the index of the `key` inside the `elements`
+    list. The `elements` list is assumed to be sorted in
+    ascending order. Returns -1 when the key element is not
+    found
+    """
+    return -1
+

fc5eefb71bc795ac67e2051e7dc164f085385651
geethalladi <geeth.alladi@freshworks.com>
2020-12-24 10:30:00 +0530

    Writing the implementation for movie_scheduling problem

--- algo/movie_scheduling.py
+++ algo/movie_scheduling.py
@@ -8,6 +8,9 @@ current movie.
 
 from typing import NamedTuple, List
 
+import logging as log
+log.basicConfig(level=log.DEBUG)
+
 
 class Movie(NamedTuple):
     """Movie data type"""
@@ -25,9 +28,22 @@ def find_maximum_subset(movies: List[Movie]) -> List[Movie]:
     gets more time to chose the rest of the movies
     """
     # Sort the movies, based on the end date (ascending order)
-    # movies = sorted(movies)
+    movies = sorted(movies, key=lambda movie: movie.end)
+    log.info(movies)
+
+    # begin is the first day where I am free
+    begin, result = 1, []
 
     # reduce this list without any collision
     # resolving towards the movie that ends first
+    for m in movies:
+        # if start is after (or equal to) begin
+        if m.start >= begin:
+            # check if the input is really valid
+            # can be turned off in production
+            assert m.start <= m.end
+            result.append(m)
+            # the new begin is one day after this movie ends
+            begin = m.end + 1
 
-    return movies
+    return result

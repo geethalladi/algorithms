641638bfa7a7a1d1d5bf8a6456352beca79fddd9
geethalladi <geeth.alladi@freshworks.com>
2021-03-08 20:14:58 +0530

    Adding TODOs in graph methods

--- algo/graphs/graph.py
+++ algo/graphs/graph.py
@@ -1,19 +1,35 @@
+"""
+Graph ADT
+"""
 from typing import Dict
 
 # TODO: This should be a sibling package
 from algo.graphs.vertex import Vertex
+
 
 class Graph:
     """
     An abstract data type for representing Graphs.
     This implementation uses the adjacency list representation.
     """
-    
+
     def __init__(self):
         """
         Create a graph instance based on adjacency list
         """
         self.vertices: Dict[id, Vertex] = {}
-        self.numVertices: int = 0
+        self.num_vertices: int = 0
 
-print(Graph)
+    def view(self):
+        """
+        Graphically represent the graph
+        """
+        # TODO: Update the implementation
+        return None
+
+    def add_vertex(self, key: str):
+        """
+        Add the given vertex to the Graph
+        """
+        # TODO: Adding Vertex
+        return None

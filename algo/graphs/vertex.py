c53f5d048d3cae7768e214488903925c58abe09d
geethalladi <geeth.alladi@freshworks.com>
2021-03-08 19:11:10 +0530

    Adding add_edge method

--- algo/graphs/vertex.py
+++ algo/graphs/vertex.py
@@ -1,26 +1,39 @@
+"""
+Vertex abstract data type
+"""
+
 from typing import Dict
 
 import logging as log
 log.basicConfig(level=log.INFO)
 
+
 class Vertex:
     """
     Vertex using adjacency list representation
     """
+    _id: str
+    _connected_to: Dict[str, int]
 
     # TODO: How to qualify self here
     def __init__(self, key: str):
         """
         Initialize the Vertex instance
         """
-        self.id: str = key
-        self.connectedTo: Dict[str, int] = {}
+        self._id = key
+        self._connected_to = {}
 
-
-    def getId(self) -> int:
+    def get_id(self) -> str:
         """
         Return the identifier of this vertex
         """
-        return self.id
-        
+        return self._id
 
+    def add_edge(self, other: 'Vertex', weight: int = 1,
+                 directed: bool = False):
+        """
+        Add an edge between this and the given vertex
+        """
+        self._connected_to[other.get_id()] = weight
+        if directed:
+            other.add_edge(self, weight, directed)
--- tests/graphs/test_vertex.py
+++ tests/graphs/test_vertex.py
@@ -5,11 +5,18 @@
 
 from algo.graphs.vertex import Vertex
 
+
 class TestVertex:
     """
     A Test Suite for Vertex
     """
+
     def test_empty(self):
-        v = Vertex('v')
-        assert v != None
-        
+        v1 = Vertex('v')
+        assert v1 is not None
+
+    def test_add_edge(self):
+        v1 = Vertex('a')
+        v2 = Vertex('b')
+        v1.add_edge(v2)
+        assert v2 is not None

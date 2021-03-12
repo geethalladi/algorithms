a0c3fe90cbf1b60ec8daebc17367bac0d75d1b91
geethalladi <geeth.alladi@freshworks.com>
2021-03-12 21:44:40 +0530

    Adding new vertex in the graph

--- algo/graphs/graph.py
+++ algo/graphs/graph.py
@@ -1,6 +1,8 @@
 """
 Graph ADT
 """
+import logging as log
+
 from typing import Dict
 
 # TODO: This should be a sibling package
@@ -13,23 +15,31 @@ class Graph:
     This implementation uses the adjacency list representation.
     """
 
+    vertices: Dict[str, Vertex]
+    num_vertices: int
+
     def __init__(self):
         """
         Create a graph instance based on adjacency list
         """
-        self.vertices: Dict[id, Vertex] = {}
-        self.num_vertices: int = 0
+        self.vertices = {}
+        self.num_vertices = 0
+
+    def add_vertex(self, key: str) -> Vertex:
+        """
+        Add the given vertex to the Graph
+        """
+        assert len(key) > 0, "Invalid Vertex Key"
+        assert key not in self.vertices, "Duplicate Vertex Key found"
+        log.debug('Add vertex %s', key)
+        self.num_vertices += 1
+        result: Vertex = Vertex(key)
+        self.vertices[key] = result
+        return result
 
     def view(self):
         """
         Graphically represent the graph
         """
         # TODO: Update the implementation
-        return None
-
-    def add_vertex(self, key: str):
-        """
-        Add the given vertex to the Graph
-        """
-        # TODO: Adding Vertex
         return None

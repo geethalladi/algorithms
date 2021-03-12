6df62313efff59bded7f9c2c83fe0f9e85d0bcff
geethalladi <geeth.alladi@freshworks.com>
2021-03-12 22:36:19 +0530

    Adding more unit tests to Graphs

--- tests/graphs/test_graph.py
+++ tests/graphs/test_graph.py
@@ -15,11 +15,16 @@ class TestGraph:
     A: Vertex
     B: Vertex
     C: Vertex
+    D: Vertex
+    E: Vertex
 
     def setup_method(self):
         self.G = Graph()
         self.A = self.G.add_vertex('a')
         self.B = self.G.add_vertex('b')
+        self.C = self.G.add_vertex('c')
+        self.D = self.G.add_vertex('d')
+        self.E = self.G.add_vertex('e')
 
     def test_empty(self):
         assert self.G is not None
@@ -43,3 +48,26 @@ class TestGraph:
         assert self.B in self.A.get_connections()
         assert self.A not in self.B.get_connections()
         assert self.A.get_weight(self.B) == 5
+
+    def test_many_vertices(self):
+        self.G.add_edge(self.A, self.B, 5, True)
+        self.G.add_edge(self.B, self.C, 6, False)
+        self.G.add_edge(self.D, self.E, 5, False)
+        self.G.add_edge(self.E, self.A, 6, False)
+
+        assert self.G.num_vertices == 5
+        assert set([self.A, self.B, self.C,
+                    self.D, self.E]) == set(self.G)
+
+    def test_many_vertices_edges(self):
+        self.G.add_edge(self.A, self.B, 5, True)
+        self.G.add_edge(self.B, self.C, 6, False)
+        self.G.add_edge(self.D, self.E, 5, False)
+        self.G.add_edge(self.E, self.A, 6, False)
+
+        assert self.A.get_weight(self.B) == 5
+        assert self.A.get_weight(self.E) == 6
+        assert self.B.get_weight(self.C) == 6
+        assert self.C.get_weight(self.B) == 6
+        assert self.D.get_weight(self.E) == 5
+        assert self.E.get_weight(self.D) == 5

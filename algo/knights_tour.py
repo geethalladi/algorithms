387e1c022382fbd8ae30b34f045d3f481be3b710
geethalladi <geeth.alladi@freshworks.com>
2021-03-20 09:35:54 +0530

    Collecting Neighbours instead of Edges

--- algo/knights_tour.py
+++ algo/knights_tour.py
@@ -3,7 +3,7 @@ Generate a knight's tour for the given n x n chess board
 """
 import logging as log
 
-from typing import NamedTuple, Sequence, Set
+from typing import NamedTuple, List, Sequence, Set, Tuple
 
 from algo.graphs.edge import Edge
 from algo.graphs.graph import Graph
@@ -17,6 +17,9 @@ class Position(NamedTuple):
     x: int
     y: int
 
+    def __repr__(self):
+        return str(self)
+
     def __str__(self):
         return '({}, {})'.format(self.x, self.y)
 
@@ -28,12 +31,20 @@ class Position(NamedTuple):
                         self.y + other.y)
 
     def within_grid(self, top_right: 'Position',
-                    bottom_left: 'Position' = Position(0, 0)):
+                    bottom_left: 'Position' = None):
         """
         Check if this position occurs within the specified grid
         """
+        if not bottom_left:
+            # Use origin if bottom_left is not specified
+            bottom_left = Position(0, 0)
+
         return (bottom_left.x <= self.x <= top_right.x) and (
             bottom_left.y <= self.y <= top_right.y)
+
+
+# declaring a type for neighbour
+Neighbour = Tuple[Position, Position]
 
 
 def knights_tour(size: int) -> Sequence[Position]:
@@ -58,33 +69,45 @@ def __create_knights_tour_graph(size: int) -> IGraph:
     """
     log.info('Creating KT Graph of size %s', size)
 
-    edges: Set[Edge] = set()
+    neighbours: Set[Neighbour] = set()
     for i in range(0, size):
         for j in range(0, size):
-            edges.update(__create_edges_for(Position(i, j), size))
+            neighbours.update(__create_edges_for(Position(i, j), size))
 
-    log.info('Created edge set of size %s', len(edges))
+    edges: List[Edge] = [Edge(str(p1), str(p2)) for (p1, p2) in neighbours]
+
+    log.info('Created edge set of size %s', len(neighbours))
+    log.info('Edge list %s', edges)
     return Graph.build('Knights Tour {}'.format(size),
                        edges,
                        directed=False)
 
 
-def __create_edges_for(pos: Position, size: int) -> Set[Edge]:
+def __create_edges_for(pos: Position, size: int) -> Set[Neighbour]:
     log.info('Creating neighbours for %s inside board of size %s', pos, size)
     possiblities = {
-        (-2, -1), (-2, 1), (2, -1), (2, 1),
-        (-1, -2), (-1, 2), (1, -2), (1, 2)
+        (-2, -1), (-2, 1),
+        (2, -1), (2, 1),
+        (-1, -2), (-1, 2),
+        (1, -2), (1, 2)
     }
-    result: Set[Edge] = set()
+    grid: Position = Position(size - 1, size - 1)
+
+    result: Set[Neighbour] = set()
     for p in possiblities:
         other = pos.displace(Position(*p))
-        if pos > other:
-            # Always the left end of the edge
-            # should be greater, to avoid
-            # duplicate edges
-            other, pos = pos, other
+        if not other.within_grid(grid):
+            # Leave it if it falls outside
+            continue
+        # if pos > other:
+        #     # Always the left end of the edge
+        #     # should be greater, to avoid
+        #     # duplicate edges
+        #     other, pos = pos, other
 
-        result.add(Edge(str(pos), str(other)))
+        result.add((pos, other))
+
+    log.info('Result is %s', result)
     return result
 
 

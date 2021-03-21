5a42092d07dc3d9c5dccbd556c2139ce5188a4c0
geethalladi <geeth.alladi@freshworks.com>
2021-03-21 17:02:09 +0530

    Vertex with description

--- algo/graphs/graph_view.py
+++ algo/graphs/graph_view.py
@@ -32,11 +32,11 @@ class GraphViewMixin:
         Add nodes to the dot representation
         """
         log.debug('Adding nodes to dot representation')
-        name: str = vertex.id
-        # if vertex.discovery != 0 or vertex.finish != 0:
-        #     name = '({}, {}, {})'.format(vertex.id, vertex.discovery,
-        #                                  vertex.finish)
-        dot.node(name, name, color=vertex.get_color())
+        desc: str = vertex.id
+        if vertex.discovery != 0 or vertex.finish != 0:
+            desc = '({}, {}, {})'.format(vertex.id, vertex.discovery,
+                                         vertex.finish)
+        dot.node(vertex.id, desc, color=vertex.get_color())
 
     @classmethod
     def __is_edge_required(cls, source: Vertex, dest: Vertex, directed: bool):

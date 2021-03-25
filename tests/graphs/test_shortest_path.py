1b25d4dc774b839904a98627470c092d1a1bf9e0
geethalladi <geeth.alladi@freshworks.com>
2021-03-25 20:47:21 +0530

    Updating tests for parent and distance

--- tests/graphs/test_shortest_path.py
+++ tests/graphs/test_shortest_path.py
@@ -3,14 +3,19 @@
 # pylint: disable=missing-function-docstring
 # pylint: disable=no-self-use
 
-from algo.graphs.shortest_path import dijkstra
+from typing import Sequence
+
+from algo.graphs.edge import Edge
 from algo.graphs.graph import Graph
+from algo.graphs.igraph import IGraph
+from algo.graphs.shortest_path import dijkstra
 
 
 class TestDijkstra:
     """
     A Test Suite for dijkstra
     """
+    DG: IGraph
 
     def setup_method(self):
         edges: Sequence[Edge] = [
@@ -28,10 +33,20 @@ class TestDijkstra:
         self.DG: IGraph = Graph.build('test_digraph_view',
                                       edges, directed=False)
 
-    def test_sample(self):
+    def test_distance(self):
+        self.DG = dijkstra(self.DG, 'U')
         assert self.DG.get_vertex('U').distance == 0
         assert self.DG.get_vertex('V').distance == 2
-        assert self.DG.get_vertex('W').distance == 5
+        assert self.DG.get_vertex('W').distance == 3
         assert self.DG.get_vertex('X').distance == 1
         assert self.DG.get_vertex('Y').distance == 2
         assert self.DG.get_vertex('Z').distance == 3
+
+    def test_parent(self):
+        self.DG = dijkstra(self.DG, 'U')
+        assert self.DG.get_vertex('U').parent is None
+        assert self.DG.get_vertex('V').parent.id == 'U'
+        assert self.DG.get_vertex('W').parent.id == 'Y'
+        assert self.DG.get_vertex('X').parent.id == 'U'
+        assert self.DG.get_vertex('Y').parent.id == 'X'
+        assert self.DG.get_vertex('Z').parent.id == 'Y'

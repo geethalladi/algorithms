6ba32f9ec114b9bfa0ccceec703693b5eb7708f1
geethalladi <geeth.alladi@freshworks.com>
2021-03-27 10:44:38 +0530

    Refactoring methods

--- algo/graphs/sort.py
+++ algo/graphs/sort.py
@@ -17,14 +17,33 @@ def topological_sort(graph: IGraph) -> Sequence[Vertex]:
     """
     Topological sorting of the graph
     """
-
-    def check_cycle(source: Vertex, dest: Vertex, edge: EdgeContainer):
-        log.info('Checking cycles in %s, %s, %s', source, dest, edge)
-        if dest.get_state() == State.DISCOVERED:
-            msg = 'Cycle exists between {} and {}'.format(source, dest)
-            raise Exception(msg)
-
-    graph.set_helper(TraversalHelper(process_edge=check_cycle))
+    # TODO: has_cycle should return a boolean
+    has_cycle(graph)
     # do a Depth First Search (Forest style)
     dfs(graph)
     return sorted(graph, key=lambda v: v.finish, reverse=True)
+
+
+def has_cycle(graph: IGraph) -> bool:
+    """
+    Check if the graph has a cycle
+    """
+    graph.set_helper(TraversalHelper(process_edge=raise_back_edge))
+    return True
+
+
+def raise_back_edge(source: Vertex, dest: Vertex, edge: EdgeContainer):
+    """
+    Check if the edge is a back edge
+    """
+    log.debug('Checking back edge in %s, %s, %s', source, dest, edge)
+    if is_back_edge(source, dest):
+        msg = 'Backend edge exists between {} and {}'.format(source, dest)
+        raise Exception(msg)
+
+
+def is_back_edge(_: Vertex, dest: Vertex) -> bool:
+    """
+    Check if the given edge is a backedge
+    """
+    return dest.get_state() == State.DISCOVERED

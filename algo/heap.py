282ecfbb2db37332dd067eeb34840ac4e6396f33
geethalladi <geeth.alladi@freshworks.com>
2021-03-29 15:53:36 +0530

    Initial implementation of heap

--- algo/heap.py
+++ algo/heap.py
@@ -2,9 +2,158 @@
 Module for implementing min and max heap data structures
 """
 
+import logging as log
+
 from typing import List
 
 __all__ = ['sort']
+
+
+class Heap:
+    """
+    Heap as a data structure
+    """
+
+    # entries for storing the elements
+    # using 1-based index
+    entries: List[int]
+    length: int
+
+    def __init__(self, reverse: bool = False):
+        self.entries = []
+        self.length = 0
+        self.reverse = reverse
+
+    def insert(self, element: int):
+        """
+        Insert the element into the heap
+        """
+        self.length += 1
+        # ensure the tree order is maintained
+        self.entries[self.length] = element
+        # ensure the heap order is maintained
+        self._bubble_up(self.length)
+
+    def empty(self) -> bool:
+        """
+        Check if the heap is empty
+        """
+        return self.length <= 0
+
+    def get(self) -> int:
+        """
+        Get the dominant element
+        """
+        assert not self.empty(), 'Empty Heap'
+        result: int = self.entries[1]
+        # maintains the tree order
+        self.entries[1] = self.entries[self.length]
+        self.length -= 1
+        self._bubble_down(1)
+        return result
+
+    def _bubble_up(self, index: int):
+        """
+        Bubble up till it reaches its equilibrium
+        based on the dominance relation
+        """
+        if index == 1:
+            log.debug('Reached the top after bubbling up')
+            return
+        parent = self._parent(index)
+        if self._is_dominant(index, parent):
+            # If more dominant than the parent
+            self._swap(index, parent)
+            # tail call recursion
+            self._bubble_up(parent)
+
+    def _bubble_down(self, index: int):
+        log.debug('Bubbling down from index %s', index)
+        left = self._left(index)
+        right = self._right(index)
+
+        if (left > self.length) and (right > self.length):
+            # leaf node
+            log.debug('Reached a leaf node after bubbling down')
+            return
+
+        d: int = self._find_dominant(index, left, right)
+        if d == index:
+            # d is dominant when compared to its children
+            # stoping bubbling down
+            return
+
+        # Swap with the dominant child
+        # and continue bubbling down
+        self._swap(d, index)
+        self._bubble_down(d)
+
+    def _find_dominant(self, index, left, right):
+        assert 1 <= index <= self.length, 'Index {} is not inside heap'.format(
+            index)
+
+        # For leaf node, return the index
+        if (left > self.length) and (right > self.length):
+            return index
+
+        # checking the dominance with left child
+        result: int = index
+        if self._is_dominant(left, index):
+            result = left
+
+        # no right child then return the result
+        if (right > self.length):
+            return result
+
+        # both the child exists
+        # and the right dominates
+        if self._is_dominant(right, result):
+            result = right
+
+        return result
+
+    def _is_dominant(self, i, j) -> bool:
+        """
+        Returns true if index 'i' is more dominant
+        than index 'j'
+        """
+        assert (1 <= i <= self.length), 'Index {} is not within heap'.format(i)
+        assert (1 <= j <= self.length), 'Index {} is not within heap'.format(j)
+
+        return self._is_dominant_value(self.entries[i], self.entries[j])
+
+    # pylint: disable=invalid-name
+    def _is_dominant_value(self, x: int, y: int) -> bool:
+        """
+        Check if value 'x' dominates value 'y'
+        """
+        if self.reverse:
+            # check the other direction (max heap)
+            return x > y
+        # min heap
+        return x < y
+
+    def _swap(self, i, j):
+        """
+        Swap the value at both the indices
+        """
+        assert (1 <= i <= self.length), 'Index {} is not within heap'.format(i)
+        assert (1 <= j <= self.length), 'Index {} is not within heap'.format(j)
+
+        self.entries[i], self.entries[j] = self.entries[j], self.entries[i]
+
+    @classmethod
+    def _parent(cls, index: int) -> int:
+        # floor(n, 2)
+        return index // 2
+
+    @classmethod
+    def _left(cls, index: int) -> int:
+        return index * 2
+
+    @classmethod
+    def _right(cls, index: int) -> int:
+        return (2 * index) + 1
 
 
 def sort(values: List[int]) -> List[int]:

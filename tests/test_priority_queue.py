d3378c7aa1d89de302e119cec6bb3dcb602deea8
geethalladi <geeth.alladi@freshworks.com>
2021-03-29 19:46:41 +0530

    Scaffolding test suite for PriorityQueue

---
+++ tests/test_priority_queue.py
@@ -0,0 +1,15 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=no-self-use
+
+from algo.priority_queue import PriorityQueue
+
+
+class TestPriorityQueue:
+    """
+    A Test Suite for PriorityQueue
+    """
+
+    def test_empty(self):
+        assert 1 == 1

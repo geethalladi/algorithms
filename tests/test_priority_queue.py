21a21c27419bf84b1ca32131707b08e9993033d9
geethalladi <geeth.alladi@freshworks.com>
2021-03-29 20:18:20 +0530

    Adding tests for Priority Queue

--- tests/test_priority_queue.py
+++ tests/test_priority_queue.py
@@ -11,5 +11,48 @@ class TestPriorityQueue:
     A Test Suite for PriorityQueue
     """
 
-    def test_empty(self):
-        assert 1 == 1
+    def test_descending(self):
+        pq = PriorityQueue()
+        pq.insert('A', 1, None)
+        pq.insert('B', 2, None)
+        pq.insert('C', 3, None)
+
+        assert pq.get() == ('C', None)
+        assert pq.get() == ('B', None)
+        assert pq.get() == ('A', None)
+
+    def test_ascending(self):
+        pq = PriorityQueue()
+        pq.insert('A', -1, None)
+        pq.insert('B', -2, None)
+        pq.insert('C', -3, None)
+
+        assert pq.get() == ('A', None)
+        assert pq.get() == ('B', None)
+        assert pq.get() == ('C', None)
+
+    def test_descending_reversed(self):
+        pq = PriorityQueue(reverse=True)
+        pq.insert('A', 1, None)
+        pq.insert('B', 2, None)
+        pq.insert('C', 3, None)
+
+        assert pq.get() == ('A', None)
+        assert pq.get() == ('B', None)
+        assert pq.get() == ('C', None)
+
+        assert pq.empty()
+
+    def test_update(self):
+        pq = PriorityQueue()
+        pq.insert('A', 1, None)
+        pq.insert('B', 2, None)
+        pq.insert('C', 3, None)
+        pq.insert('D', 4, None)
+
+        pq.update('D', 0)
+
+        assert pq.get() == ('C', None)
+        assert pq.get() == ('B', None)
+        assert pq.get() == ('A', None)
+        assert pq.get() == ('D', None)

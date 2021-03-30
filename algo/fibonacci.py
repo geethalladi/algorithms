cee3da50c35120f8d550f4e1097aa41460ea77a5
geethalladi <geeth.alladi@freshworks.com>
2021-03-30 08:19:12 +0530

    Scaffolding fibonacci series

---
+++ algo/fibonacci.py
@@ -0,0 +1,17 @@
+"""
+Different implementation of fibonacci series
+"""
+
+import logging as log
+
+
+def fib_recursive(n: int) -> int:
+    """
+    Naive recursive implementation of fibonacci
+    """
+    assert n >= 0, 'Negative index {} for computing Fib'.format(n)
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_recursive(n-1) + fib_recursive(n-2)
---
+++ tests/test_fibonacci.py
@@ -0,0 +1,32 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=no-self-use
+
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=no-self-use
+
+from typing import List
+
+from algo.fibonacci import fib_recursive as fr
+
+
+class TestFibonacci:
+    """
+    A Test Suite for Fibonacci
+    """
+
+    result: List[int]
+
+    def setup_method(self):
+        self.result = [0, 1, 1, 2, 3, 5, 8, 13,
+                       21, 34, 55, 89, 144, 233, 377, 610]
+
+    def test_recursive_fib(self):
+        assert fr(0) == 0
+        assert fr(1) == 1
+
+        for i in range(0, len(self.result)):
+            assert fr(i) == self.result[i]

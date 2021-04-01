ffc50d6987d7fbcad5bc7d5116e4c469c7b22eb9
geethalladi <geeth.alladi@freshworks.com>
2021-04-01 13:11:55 +0530

    Testing editing distance

--- tests/test_edit_distance.py
+++ tests/test_edit_distance.py
@@ -12,7 +12,25 @@ class TestEditDistance:
     """
 
     def test_same(self):
-        assert ed('hello', 'hello') == (0, '')
+        assert ed('hello', 'hello') == (0, 'MMMMM')
+
+    def test_substitute(self):
+        assert ed('ADCD', 'ABCE') == (2, 'MSMS')
 
     def test_insert(self):
-        assert ed('ello', 'hello') == (1, 'Immmm')
+        assert ed('hell', 'hello') == (1, 'MMMMI')
+
+    def test_delete(self):
+        assert ed('hello', 'hell') == (1, 'MMMMD')
+
+    def test_insert_middle(self):
+        assert ed('hll', 'hello') == (2, 'MIMMI')
+
+    def test_delete_middle(self):
+        assert ed('hello', 'hll') == (2, 'MDMMD')
+
+    def test_insert_begin(self):
+        assert ed('llo', 'hello') == (2, 'IIMMM')
+
+    def test_original(self):
+        assert ed('thou-shalt-not', 'you-should-not') == (5, 'DSMMMMMISMSMMMM')

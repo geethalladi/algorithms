d6cf1e2b06ab3e34f2ad1838bfe1f69941c4a126
geethalladi <geeth.alladi@freshworks.com>
2021-04-06 17:50:21 +0530

    Writing a recursive version

--- algo/egg_dropping_lc.py
+++ algo/egg_dropping_lc.py
@@ -3,6 +3,9 @@ Given k eggs and a 'n' story building, find the minimum
 droppings to find the smallest floor, where the egg breaks
 """
 
+import logging as log
+
+import functools
 import sys
 
 from typing import List
@@ -14,6 +17,26 @@ class Solution:  # pylint: disable=too-many-public-methods
     def __get(self, i, j, x, table) -> int:
         return max(table[i-1][x-1],
                    table[i][j-x]) + 1
+    @functools.lru_cache
+    def superEggDropR(self, eggs: int, floors: int) -> int:
+        assert eggs >= 0 and floors >= 0, 'Invalid condition'
+        if eggs <= 0:
+            return 0
+        if floors <= 1:
+            return 1
+        if eggs == 1:
+            return floors
+
+        log.info('Call %s, %s', eggs, floors)
+        result = sys.maxsize
+        for x in range(1, floors + 1):
+            c1, c2 = self.superEggDropR(
+                eggs - 1, x - 1), self.superEggDropR(eggs, floors - x)
+            c = max(c1, c2) + 1
+            if c < result:
+                result = c
+
+        return result
 
     def superEggDrop(self, eggs: int, floors: int) -> int:
         """

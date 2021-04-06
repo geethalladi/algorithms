55327d22e1e8499fe4a410f8d5b8505a90c58609
geethalladi <geeth.alladi@freshworks.com>
2021-04-06 06:43:57 +0530

    Fixing the subtle bug

--- algo/egg_dropping.py
+++ algo/egg_dropping.py
@@ -29,18 +29,41 @@ def egg_dropping(eggs: int, floors: int) -> int:
     """
     table: List[List[Cell]] = __init_table(eggs, floors)
 
-    for i in range(1, eggs + 1):
-        for j in range(1, floors + 1):
+    for i in range(2, eggs + 1):
+        for j in range(2, floors + 1):
             # Try dropping the egg from each of the 'x' floors
             # and find its minimum
             results: List[Cell] = []
-            for x in range(1, floors):
+            for x in range(1, floors + 1):
                 result: Cell = __compute_cost(x, i, j, table)
                 results.append(result)
 
             table[i][j] = __find_minimum_cost(results)
+            log.info('Cost for %s eggs with %s floors is %s',
+                     i, j, table[i][j].moves)
 
     return table[eggs][floors].moves
+
+
+def __init_table(eggs: int, floors: int) -> List[List[Cell]]:
+    result: List[List[Cell]] = []
+    empty: Cell = Cell()
+
+    # Init an empty table
+    for i in range(0, eggs + 1):
+        result.append([])
+        for _ in range(0, floors + 1):
+            result[i].append(empty)
+
+    # with a single egg, and n floors, the ans is n
+    for j in range(0, floors + 1):
+        result[1][j] = Cell(1, j, (1, j - 1))
+
+    # on a single floor with k eggs, the answer is 1
+    for i in range(0, eggs + 1):
+        result[i][1] = Cell(i, 1, (-1, -1))
+
+    return result
 
 
 # pylint: disable=invalid-name
@@ -49,6 +72,8 @@ def __compute_cost(x: int, eggs: int, floors: int, table: List[List[Cell]]) -> C
     c: Cell = table[eggs - 1][x - 1]
     # if it does not
     d: Cell = table[eggs][floors - x]
+
+    log.info('From %s floor, %s, %s', x, c, d)
 
     # what is the maximum moves when the egg is thrown from floor 'x'
     # 1+ to account for throwing from floor 'x'
@@ -65,24 +90,3 @@ def __find_minimum_cost(results: List[Cell]) -> Cell:
         if results[i].moves < mn.moves:
             mn = results[i]
     return mn
-
-
-def __init_table(eggs: int, floors: int) -> List[List[Cell]]:
-    result: List[List[Cell]] = []
-    empty: Cell = Cell()
-
-    # Init an empty table
-    for i in range(0, eggs + 1):
-        result.append([])
-        for _ in range(0, floors + 1):
-            result[i].append(empty)
-
-    # with a single egg, and n floors, the ans is n
-    for j in range(0, floors):
-        result[1][j] = Cell(1, j, (1, j - 1))
-
-    # on a single floor with k eggs, the answer is 1
-    for i in range(0, eggs):
-        result[i][1] = Cell(i, 1, (-1, -1))
-
-    return result

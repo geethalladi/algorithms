84789dfd441ecdb36b4c7a39700a0e0d7e9a326f
geethalladi <geeth.alladi@freshworks.com>
2021-04-06 08:41:22 +0530

    Updating time

--- tests/test_egg_dropping.py
+++ tests/test_egg_dropping.py
@@ -6,7 +6,7 @@
 import logging as log
 import time
 
-from algo.egg_dropping import egg_dropping
+from algo.egg_dropping_lc import egg_dropping
 
 
 class TestEggDropping:
@@ -17,6 +17,6 @@ class TestEggDropping:
     def test_empty(self):
         # assert egg_dropping(2, 6) == 3
         start = time.process_time()
-        assert egg_dropping(4, 1000) == 13
+        assert egg_dropping(4, 2000) == 16
         end = time.process_time()
         log.info('Took %s to complete egg dropping', (end - start))

3aad6c88f0d65b0401817d456aa29c986ebe7f57
geethalladi <geeth.alladi@freshworks.com>
2021-04-07 10:08:22 +0530

    Updating unit tests to test both optimized and original version

--- tests/test_egg_dropping.py
+++ tests/test_egg_dropping.py
@@ -6,7 +6,8 @@
 import logging as log
 import time
 
-from algo.egg_dropping_lc import egg_dropping as eg
+from algo.egg_dropping import egg_dropping as eg
+from algo.egg_dropping_optimized import egg_dropping as ego
 
 
 class TestEggDropping:  # pylint: disable=too-few-public-methods
@@ -14,11 +15,20 @@ class TestEggDropping:  # pylint: disable=too-few-public-methods
     A Test Suite for egg_dropping
     """
 
-    def test_samples(self):
+    def test_original(self):
         start = time.process_time()
         assert eg(2, 2) == 2
         assert eg(2, 6) == 3
         assert eg(4, 2000) == 16
         assert eg(4, 5000) == 19
+        end = time.process_time()
+        log.info('Took %s to complete egg dropping', (end - start))
+
+    def test_optimized(self):
+        start = time.process_time()
+        assert ego(2, 2) == 2
+        assert ego(2, 6) == 3
+        assert ego(4, 2000) == 16
+        assert ego(4, 5000) == 19
         end = time.process_time()
         log.info('Took %s to complete egg dropping', (end - start))

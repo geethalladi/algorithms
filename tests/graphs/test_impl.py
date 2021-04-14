b5afa7a564b676c0f5730c32c3b034c42b16c552
geethalladi <geeth.alladi@freshworks.com>
2021-04-14 14:51:16 +0530

    Test cases for edge classification

--- tests/graphs/test_impl.py
+++ tests/graphs/test_impl.py
@@ -3,12 +3,14 @@
 # pylint: disable=missing-function-docstring
 # pylint: disable=no-self-use
 
+import logging as log
+
 from typing import Sequence
 
-from algo.graphs.edge import EdgeInput
+from algo.graphs.edge import EdgeInput, EdgeType as ET
 from algo.graphs.graph import Graph
 from algo.graphs.igraph import IGraph
-from algo.graphs.impl import is_bipartite
+from algo.graphs.impl import is_bipartite, edge_classifier
 
 
 class TestEdgeClassifier:
@@ -16,8 +18,94 @@ class TestEdgeClassifier:
     A Test Suite for edge classification
     """
 
-    def test(self):
-        assert 1 == 1
+    def test_undirected_tree_back(self):
+        edges: Sequence[EdgeInput] = [
+            ('1', '2'),
+            ('1', '5'),
+            ('1', '6'),
+            ('2', '3'),
+            ('2', '5'),
+            ('3', '4'),
+            ('4', '5'),
+            ('5', '6'),
+            ('2', '6'),
+            ('2', '4')
+        ]
+        g: IGraph = Graph.build('test_edge_classification',
+                                edges, directed=False)
+        edge_classifier(g, '1')
+        # undirected graph should only have
+        # (tree, back) edges
+        for e in g.edges():
+            assert e.type in [ET.TREE, ET.BACK]
+
+    def test_undirected(self):
+        edges: Sequence[EdgeInput] = [
+            ('1', '2'),
+            ('1', '5'),
+            ('1', '6'),
+            ('2', '3'),
+            ('2', '5'),
+            ('3', '4'),
+            ('4', '5')
+        ]
+        g: IGraph = Graph.build('test_graph_builder',
+                                edges, directed=False)
+
+        result = {
+            '(1, 2)': ET.TREE,
+            '(2, 3)': ET.TREE,
+            '(3, 4)': ET.TREE,
+            '(4, 5)': ET.TREE,
+            '(1, 6)': ET.TREE,
+            '(2, 5)': ET.BACK,
+            '(1, 5)': ET.BACK
+        }
+        edge_classifier(g, '1')
+        for e in g.edges():
+            assert e.type == result[f'({e.source}, {e.dest})']
+
+    def test_directed(self):
+        edges: Sequence[EdgeInput] = [
+            ('1', '2'),
+            ('2', '3'),
+            ('1', '3'),
+            ('2', '4'),
+            ('4', '1')
+        ]
+        g: IGraph = Graph.build('test_graph_builder',
+                                edges, directed=True)
+
+        result = {
+            '(1, 2)': ET.TREE,
+            '(2, 3)': ET.TREE,
+            '(1, 3)': ET.FORWARD,
+            '(2, 4)': ET.TREE,
+            '(4, 1)': ET.BACK
+        }
+        edge_classifier(g, '1')
+        for e in g.edges():
+            log.info(e)
+            assert e.type == result[f'({e.source}, {e.dest})']
+
+    def test_directed_cross(self):
+        edges: Sequence[EdgeInput] = [
+            ('1', '2'),
+            ('2', '3'),
+            ('4', '2')
+        ]
+        g: IGraph = Graph.build('test_graph_builder',
+                                edges, directed=True)
+
+        result = {
+            '(1, 2)': ET.TREE,
+            '(2, 3)': ET.TREE,
+            '(4, 2)': ET.CROSS
+        }
+        edge_classifier(g, '1')
+        for e in g.edges():
+            log.info(e)
+            assert e.type == result[f'({e.source}, {e.dest})']
 
 
 class TestBiPartite:
